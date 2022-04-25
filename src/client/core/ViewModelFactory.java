4e6b3dac3a6de5b951b595091089ce8611388b57
Uafa <315211@via.dk>
2022-04-25 19:39:10 +0200

    Created packages and made the body of the core (need the interfaces to continue working)

---
+++ src/client/core/ViewModelFactory.java
@@ -0,0 +1,12 @@
+package client.core;
+
+
+public class ViewModelFactory
+{
+  private ModelFactory modelFactory;
+
+  public ViewModelFactory(ModelFactory modelFactory)
+  {
+    this.modelFactory = modelFactory;
+  }
+}
