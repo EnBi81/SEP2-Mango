dc7c6179e75161981e5a9ca9e3df6018a65284c2
simonfrayer <89986425+simonfrayer@users.noreply.github.com>
2022-04-25 23:09:29 +0200

    RegisterViewController and RegisterView

---
+++ src/client/view/register/RegisterViewController.java
@@ -0,0 +1,29 @@
+package client.view.register;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class RegisterViewController implements ViewController
+{
+
+  @FXML private TextField firstName;
+  @FXML private RadioButton radioCustomer;
+  @FXML private RadioButton radioEmployee;
+  @FXML private TextField lastName;
+  @FXML private TextField username;
+  @FXML private PasswordField password;
+  @FXML private PasswordField passwordRepeat;
+  @FXML private Label errorMessage;
+
+  @FXML private void onBack(ActionEvent actionEvent)
+  {
+  }
+
+  @FXML private void onRegister(ActionEvent actionEvent)
+  {
+  }
+}
