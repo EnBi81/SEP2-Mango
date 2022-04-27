7b0ef55ca3e4e50720e9c0068120e9be02fe0ec4
Enbi81 <wotng2002@gmail.com>
2022-04-27 09:41:08 +0200

    Added Serializable to LoginRequest

--- src/transferobjects/LoginRequest.java
+++ src/transferobjects/LoginRequest.java
@@ -2,7 +2,9 @@ package transferobjects;
 
 import shared.UserType;
 
-public class LoginRequest
+import java.io.Serializable;
+
+public class LoginRequest implements Serializable
 {
   private String firstName;
   private String lastName;
